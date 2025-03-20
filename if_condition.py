0cca2a2c2016bfd73919175ee7adf452eead0a60
manojz4 <manoj444padvi@gmail.com>
2025-03-20 12:37:35 +0530

    if condition 1st q

---
+++ if_condition.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number :"))
+if num % 2 == 0:
+    print ("The number is even")
+
+#    Odd or Even:
+#    Check if a number is even using only if. If the number is even, print it.
+#    Enter a number :6
+#    The number is even
