03c51d728f29c58f1bea3d1946cb8fb6a900e3c3
manojz4 <manoj444padvi@gmail.com>
2025-03-20 12:53:04 +0530

    divisible

---
+++ div.py
@@ -0,0 +1,12 @@
+#Divisible by 7:
+#Write a program to check if a number is divisible by 7. If it is, print "Divisible by 7".
+
+num = int (input("enter a number :"))
+if num % 7 ==0:
+    print ("number is divisible is 7")
+
+
+#    enter a number :49
+#number is divisible is 7
+#PS D:\all python projects
+#enter a number :5
