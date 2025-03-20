a14474aa33e318f6e23ad137623099334d617afd
manojz4 <manoj444padvi@gmail.com>
2025-03-20 13:55:08 +0530

    uppercase

---
+++ uppercase.py
@@ -0,0 +1,11 @@
+#Check Uppercase:
+#Given a character, check if it is an uppercase letter using only if. If yes, print "Uppercase".
+
+char =input("enter text :")
+if char.isupper():
+    print("Uppercase")
+
+
+#char.isupper() checks if the input is an uppercase letter.
+#If the condition is true, it prints "Uppercase".
+
