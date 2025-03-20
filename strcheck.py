678816a3f5606c88227d61694f4a1c5d3f87231a
manojz4 <manoj444padvi@gmail.com>
2025-03-20 14:29:23 +0530

    string length checker

---
+++ strcheck.py
@@ -0,0 +1,18 @@
+#String Length Check:
+#Write a program to check if the length of a given string is greater than 5. If true, print the string.
+
+var = input("enter a string :")
+if len(var) >5:
+    print(var)
+
+#enter a string :manoj
+#PS D:\all python projects
+#enter a string :manojpadvi
+#manojpadvi
+
+#####################################################
+
+#Explanation:
+#len(var) gets the length of the input string.
+#If the length is greater than 5, it prints the string.
+
