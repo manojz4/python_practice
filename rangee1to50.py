a5d26c76d679f0862df41dd1d7d78a0518439125
manojz4 <manoj444padvi@gmail.com>
2025-03-20 14:09:42 +0530

    range between 1 to 50

---
+++ rangee1to50.py
@@ -0,0 +1,12 @@
+#Number Between 1 and 50:
+#Write a program that checks if a number is between 1 and 50 (both inclusive). If it is, print the number.
+
+num = int(input("enter number :"))
+if num > 1 and num < 50:
+    print(num)
+
+
+#enter number :55
+#PS D:\all python projects
+#enter number :38
+#38
