221415f490c34e70d2e0ca4fe1534f4904875088
manojz4 <manoj444padvi@gmail.com>
2025-03-20 13:03:04 +0530

    greater

---
+++ four.py
@@ -0,0 +1,12 @@
+#Greater than 100:
+#Check if a given number is greater than 100. If true, print the number.
+
+num = int(input("Enter a number :"))
+if num > 100:
+    print(num)
+
+'''
+Enter a number :555
+555
+PS D:
+'''
