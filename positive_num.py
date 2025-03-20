813d44486b2761bc50e80bc8eda06aaca36a4d65
manojz4 <manoj444padvi@gmail.com>
2025-03-20 12:45:38 +0530

    positive number

---
+++ positive_num.py
@@ -0,0 +1,13 @@
+num = int(input("enter a number :"))
+if num > 0:
+    print("number is positive")
+
+
+#Positive Number:
+#Write a program that checks if a number is positive. If it is, print "Positive".
+
+#enter a number :-1
+#PS D:\all python projects
+#enter a number :5
+#number is positive
+
